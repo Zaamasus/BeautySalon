9c286a108e663c9b498e40a4042efcb5dba21c19
Zaamasus <samuelgomes22442@gmail.com>
2023-02-03 16:59:43 -0300

    30303

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,5 +1,37 @@
+import { CarruselServices } from "../components/CarruselServices";
+
 export function Services () {
   return(
-    <h1>teste</h1>
+    <div className="block">
+
+        <div className="text-center ">
+          <h2 className="font-sans font-bold text-4xl text-['hsla(159, 41%, 10%, 1)">
+            Serviços
+          </h2>
+
+          <p className="font-serif font-normal  text-[#767676] mt-3">
+          
+          Com mais de 10 anos no mercado, o <span className="text-green-400">Beautysalon</span>  já 
+          <br />
+          
+          conquistou clientes de inúmeros países com seus tratamentos
+          
+          <br />
+           exclusivos e totalmente naturais
+          </p>
+        </div>
+
+        
+
+
+        <div>
+
+        <CarruselServices/>
+
+        </div>
+
+
+    </div>
+
   )
 }
