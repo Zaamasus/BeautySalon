e7a85407bc85fa22b0d5af3695192e5b489dc72e
Zaamasus <samuelgomes22442@gmail.com>
2023-02-08 18:01:04 -0300

    87948

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://zaamasus.github.io/BeautySalon/",
+  build: {
+    outDir: 'build'
+    }
 })
